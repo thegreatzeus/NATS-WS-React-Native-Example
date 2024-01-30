d4f809c4ddedb5b242a17ddf67a2cd577e0c2a27
Mehrdad Moradi <me@mmoradi.ca>
2024-01-30 13:26:52 -0600

    Example app component added

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-url-polyfill/auto';
+import 'text-encoding-polyfill';
+import '@azure/core-asynciterator-polyfill'
+
+import { connect } from "nats.ws";
+import { View, Text } from 'react-native'
+import { useEffect, useState } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [nats, setNats] = useState();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    (async () => {
+      const nc = await connect({
+        servers: ["wss://demo.nats.io:8443"],
+      })
+      setNats(nc)
+      console.log("connected to NATS")
+    })();
+
+    return () => {
+      nats?.drain();
+      console.log("closed NATS connection")
+    }
+  }, [])
+
+  return (
+      <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+        {nats ? (
+            <Text>Connected to {nats?.getServer()}</Text>
+        ) : (
+            <Text>Connecting to NATS...</Text>
+        )}
+      </View>
+  )
+}
